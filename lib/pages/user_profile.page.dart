262b0fb28c6a212a0765602cd1c21589dea39bee
Nesar Wagannawar <nesar2004@gmail.com>
2025-02-22 12:46:03 +0530

    v1.6.2(User Page Dynamical Load)

--- lib/pages/user_profile.page.dart
+++ lib/pages/user_profile.page.dart
@@ -35,6 +35,11 @@ class _UserProfilePageState extends State<UserProfilePage> {
     _fetchUserData();
   }
 
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
   Future<void> _fetchUserData() async {
     DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(widget.user.uid).get();
     if (userDoc.exists) {
@@ -74,141 +79,157 @@ class _UserProfilePageState extends State<UserProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.black, Colors.black87]),
-        ),
-        child: ListView(
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // Align to the left
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 50.0, left: 16.0, right: 16.0, bottom: 16.0), // Add margin from the top
-                  child: Text(
-                    'User Profile',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 34,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
+      body: FutureBuilder(
+        future: _fetchUserData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              color: Colors.black,
+              child: Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
+              ),
+            );
+          } else {
+            return Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.black, Colors.black87]),
+              ),
+              child: ListView(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, // Align to the left
                     children: <Widget>[
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundImage: _image != null
-                            ? FileImage(_image!)
-                            : AssetImage('assets/profile_photo.jpg') as ImageProvider,
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50.0, left: 16.0, right: 16.0, bottom: 16.0), // Add margin from the top
+                        child: Text(
+                          'User Profile',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 34,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
                       ),
-                      SizedBox(width: 16),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            'Name: $userName', // Display the fetched user name
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            'Email: ${widget.user.email}',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                          ),
-                          SizedBox(height: 16),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black, // Set button color to black
-                              foregroundColor: Colors.white, // Set text color to white
-                              side: BorderSide(color: Colors.white, width: 2.0), // Add white outline
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30), // Rounded edges
-                              ),
-                              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          children: <Widget>[
+                            CircleAvatar(
+                              radius: 50,
+                              backgroundImage: _image != null
+                                  ? FileImage(_image!)
+                                  : AssetImage('assets/profile_photo.jpg') as ImageProvider,
                             ),
-                            onPressed: () async {
-                              await _fetchUserData();
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => EditProfilePage(user: widget.user)),
-                              );
-                            },
-                            child: Text('Edit Profile'),
+                            SizedBox(width: 16),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'Name: $userName', // Display the fetched user name
+                                  style: TextStyle(color: Colors.white, fontSize: 18),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  'Email: ${widget.user.email}',
+                                  style: TextStyle(color: Colors.white, fontSize: 18),
+                                ),
+                                SizedBox(height: 16),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.black, // Set button color to black
+                                    foregroundColor: Colors.white, // Set text color to white
+                                    side: BorderSide(color: Colors.white, width: 2.0), // Add white outline
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30), // Rounded edges
+                                    ),
+                                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                                  ),
+                                  onPressed: () async {
+                                    await _fetchUserData();
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => EditProfilePage(user: widget.user)),
+                                    );
+                                  },
+                                  child: Text('Edit Profile'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  'Connections:',
+                                  style: TextStyle(color: Colors.white, fontSize: 18),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  '150',
+                                  style: TextStyle(color: Colors.white, fontSize: 18),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  'Total Quipps:',
+                                  style: TextStyle(color: Colors.white, fontSize: 18),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  '75',
+                                  style: TextStyle(color: Colors.white, fontSize: 18,),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 28),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Top Quipps:',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w300,
                           ),
-                        ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: topQuips.map((quip) => ListTile(
+                            title: Text(
+                              quip,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            leading: Icon(Icons.message, color: Colors.white),
+                          )).toList(),
+                        ),
                       ),
                     ],
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            'Connections:',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            '150',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            'Total Quipps:',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            '75',
-                            style: TextStyle(color: Colors.white, fontSize: 18,),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 28),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'Top Quipps:',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: topQuips.map((quip) => ListTile(
-                      title: Text(
-                        quip,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      leading: Icon(Icons.message, color: Colors.white),
-                    )).toList(),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
